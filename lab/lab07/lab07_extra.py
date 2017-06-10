e98ef228ac6accd055340abb716786857db034e1
JiahaoTang <jiahaotang1994@gmail.com>
2017-06-11 00:18:59 +0800

    Finished the lab07

--- lab/lab07/lab07_extra.py
+++ lab/lab07/lab07_extra.py
@@ -13,8 +13,28 @@ def reverse_other(t):
     >>> t
     Tree(1, [Tree(3, [Tree(5, [Tree(8), Tree(7)]), Tree(6)]), Tree(2)])
     """
-    "*** YOUR CODE HERE ***"
 
+    """define a swap function that can reverse the root of branches"""
+    def swap(lst):
+        if len(lst) == 1:
+            lst[0] = lst[0]
+        elif len(lst) == 2:
+            tempRoot = lst[0].root
+            lst[0].root = lst[len(lst) - 1].root
+            lst[len(lst) - 1].root = tempRoot
+        elif len(lst) > 2:
+            tempRoot = lst[0].root
+            lst[0].root = lst[len(lst) - 1].root
+            lst[len(lst) - 1].root = tempRoot
+            swap(lst[1:len(lst) - 2])
+    """the main function of reverse_other"""
+    if t.branches == []:
+        return t
+    else:
+        swap(t.branches)
+        for branch in t.branches:
+            for deepbranch in branch.branches:
+                reverse_other(deepbranch)
 # Q7
 def cumulative_sum(t):
     """Mutates t where each node's root becomes the sum of all entries in the
@@ -25,7 +45,13 @@ def cumulative_sum(t):
     >>> t
     Tree(16, [Tree(8, [Tree(5)]), Tree(7)])
     """
-    "*** YOUR CODE HERE ***"
+    if t.branches == []:
+        t.root = t.root
+        return t.root
+    else:
+        for branch in t.branches:
+            cumulative_sum(branch)
+        t.root = t.root + sum([branch.root for branch in t.branches])
 
 # Q8
 def deep_map_mut(fn, link):
@@ -40,8 +66,19 @@ def deep_map_mut(fn, link):
     >>> print_link(link1)
     <9 <16> 25 36>
     """
-    "*** YOUR CODE HERE ***"
-
+    if link.rest == ():
+        if isinstance(link.first, int):
+            link.first = fn(link.first)
+        elif isinstance(link.first, Link):
+            deep_map_mut(fn, link.first)
+    else:
+        if isinstance(link.first, int):
+            link.first = fn(link.first)
+            deep_map_mut(fn, link.rest)
+        elif isinstance(link.first, Link):
+            deep_map_mut(fn, link.first)
+            deep_map_mut(fn, link.rest)
+    
 # Q9
 def has_cycle(link):
     """Return whether link contains a cycle.
@@ -57,7 +94,12 @@ def has_cycle(link):
     >>> has_cycle(u)
     False
     """
-    "*** YOUR CODE HERE ***"
+    s = link.rest
+    while s != link:
+        s = s.rest
+        if s == ():
+            return False
+    return True
 
 def has_cycle_constant(link):
     """Return whether link contains a cycle.
@@ -70,4 +112,13 @@ def has_cycle_constant(link):
     >>> has_cycle_constant(t)
     False
     """
-    "*** YOUR CODE HERE ***"
+    s = link.rest
+    while s != link:
+        s = s.rest
+        if s == ():
+            return False
+    return True
+    """This question I cannot solve so I use the same solution. 
+    The solution of the CS61A website use the double node, the
+    fast one and the slow one. Maybe this is the more efficient
+    solution. I have to learn more about efficiency"""
